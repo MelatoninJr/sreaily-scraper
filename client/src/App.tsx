6d4aa3ec004cdc8a3e91acdb191de95d62dd003b
MelatoninJr <dillon32300@gmail.com>
2023-10-21 04:31:56 +0700

    saving

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,34 @@
 import React, { useState, useEffect } from "react";
 import Card from "./components/card";
 import './App.css';
-import { useNavigate } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
-// Define the type for the expected data structure
 interface DataItem {
   title: string;
   imageSrc: string;
 }
 
 const App: React.FC = () => {
-  const itemsPerPage = 20; // Number of items to display per page
+  const { page } = useParams<{ page?: string }>();
+  const currentPageFromURL = parseInt(page || '1', 10);
+
   const [data, setData] = useState<DataItem[]>([]);
-  const [currentPage, setCurrentPage] = useState(1);
   const [loading, setLoading] = useState(false);
-  const [totalPages, setTotalPages] = useState(1);
+  const navigate = useNavigate();
+  
+  const [currentPage, setCurrentPage] = useState(currentPageFromURL);
 
   useEffect(() => {
-    // Fetch data from your API
+    navigate(`/api/scrapeSreality/${currentPage}`);
+
     const fetchData = async () => {
       try {
         setLoading(true);
-
-        // Replace this URL with the actual API endpoint
+        
         const response = await fetch(`http://localhost:3001/api/scrapeSreality/${currentPage}`);
         const result = await response.json();
-
-        setData(result.data); // Assuming your API returns an object with a "data" property
-        setTotalPages(Math.ceil(result.data.length / itemsPerPage));
+        
+        setData(result.data);
         setLoading(false);
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -36,49 +37,22 @@ const App: React.FC = () => {
     };
 
     fetchData();
-  }, []);
-
-  const handlePageClick = (pageNumber: number) => {
-    setCurrentPage(pageNumber);
-  };
+  }, [currentPage, navigate]);
 
   const handleNextPageClick = () => {
-      setCurrentPage(currentPage + 1);
-
+    setCurrentPage(currentPage + 1);
   };
 
   const handlePreviousPageClick = () => {
-      setCurrentPage(currentPage - 1);
+    setCurrentPage(currentPage - 1);
   };
-
-  const startIndex = (currentPage - 1) * itemsPerPage;
-  const endIndex = startIndex + itemsPerPage;
-  const itemsToDisplay = data.slice(startIndex, endIndex);
 
   return (
     <div className="home-container">
       <div className="container mx-auto flex justify-center">
         <h1 className="text-4xl font-semibold text-center m-8">Current Listings</h1>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {loading ? (
-          <div className="container grow mx-auto flex justify-center">
-            <p>Loading...</p>
-          </div>
-        ) : (
-          itemsToDisplay.map((item, index) => (
-            <Card
-              key={index}
-              title={item.title}
-              imageSrc={item.imageSrc}
-              description="Description goes here"
-              tags={["Fashion", "Products"]}
-              isNew={true}
-            />
-          ))
-        )}
-      </div>
-
+      <ItemsDisplay loading={loading} itemsToDisplay={data} />
       <div className="container mx-auto flex justify-center m-8">
         <button
           className="btn"
@@ -87,13 +61,33 @@ const App: React.FC = () => {
         >
           Previous Page
         </button>
-        <button
-          className="btn"
-          onClick={handleNextPageClick}
-        >
+        <button className="btn" onClick={handleNextPageClick}>
           Next Page
         </button>
       </div>
+    </div>
+  );
+};
+
+const ItemsDisplay: React.FC<{ loading: boolean, itemsToDisplay: DataItem[] }> = ({ loading, itemsToDisplay }) => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      {loading ? (
+        <div className="container grow mx-auto flex justify-center">
+          <p>Loading...</p>
+        </div>
+      ) : (
+        itemsToDisplay.map((item, index) => (
+          <Card
+            key={index}
+            title={item.title}
+            imageSrc={item.imageSrc}
+            description="Description goes here"
+            tags={["Fashion", "Products"]}
+            isNew={true}
+          />
+        ))
+      )}
     </div>
   );
 };
